c49a7bcbf81d47c65e7ee9ea3dd6e00cbbcee8a2
akayela <akayela22@gmail.com>
2024-02-21 22:57:06 +0200

    Added Serial communication

--- ch01/01_blink/blink_led/blink_led.ino
+++ ch01/01_blink/blink_led/blink_led.ino
@@ -5,7 +5,8 @@ int ledPin = 13;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(ledPin, OUTPUT); // set pin 13 as an outputpin
+  pinMode(ledPin, OUTPUT); // set pin 13 as an outputpin 
+  Serial.begin(9600);
 
 }
 
@@ -14,16 +15,26 @@ void loop() {
   for ( int i=0; i<=20; i++) {
     if (i % 2 == 0 && i < 20 ) {
   digitalWrite(ledPin, HIGH); // pull the pin voltage high
+  Serial.print("Pin: ");
+  Serial.print(ledPin);
+  Serial.print(" is On for the ");
+  Serial.print(i);
+  Serial.println("th time");
   delay(1000); // wait for 250 ms
   digitalWrite(ledPin, LOW); // pull the pin voltage low
+   Serial.print("Pin: ");
+  Serial.print(ledPin);
+  Serial.println(" is Off");
   delay(1000); // wait for 300 ms
     } else if(1 % 2 != 0 && i < 20) {
         digitalWrite(ledPin, HIGH); // pull the pin voltage high
+  Serial.println("i is odd right now.");
         delay(300); // wait for 250 ms
         digitalWrite(ledPin, LOW); // pull the pin voltage low
         delay(300); // wait for 300 ms
     } else {
       digitalWrite(ledPin, HIGH);
+      Serial.println("i is now 20");
       delay(5000);
       digitalWrite(ledPin, LOW);
       delay(5000);
